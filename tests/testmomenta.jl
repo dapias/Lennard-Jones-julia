12e16825f2944df6b69404d24056f775109a08a3
dapias <dandroveros@gmail.com>
2016-03-12 18:31:20 -0600

    Add testmomenta to check that the linear momenta of the system is conserved during the simulation

---
+++ tests/testmomenta.jl
@@ -0,0 +1,58 @@
+include("../ThermostattedLennardJones.jl")
+
+using ThermostattedLennardJones
+using FactCheck
+
+dim = 3
+#Parameters tested
+N = 343
+rho = 1.0
+T = 0.1
+dt = 0.01
+runtime = 0.1
+Q = 1.0
+
+L = cbrt(N/rho)
+
+atoms, T, K, U = ThermostattedLennardJones.initialize(L, N, T, rho)
+
+
+facts("Conserved Linear Momenta At the Beginning of the simulation ") do
+
+  momenta = Array(Float64,3)
+
+  for j in 1:dim
+    sumv = 0.0
+    for i in 1:N
+      sumv += atoms[i].p[j]
+    end
+    momenta[j] = sumv
+  end
+
+  @fact abs(momenta[1]) < 1.0e-10 --> true
+  @fact abs(momenta[2]) < 1.0e-10 --> true
+  @fact abs(momenta[3]) < 1.0e-10 --> true
+end
+
+facts("Conserved Linear Momenta After 10   steps ") do
+
+  time, energy, kinetic, potential, temperature, invariant, atoms = ThermostattedLennardJones.run(runtime, rho, dt, T, N, Q)
+
+  momenta = Array(Float64,3)
+
+  for j in 1:dim
+    sumv = 0.0
+    for i in 1:N
+      sumv += atoms[i].p[j]
+    end
+    momenta[j] = sumv
+  end
+
+  @fact abs(momenta[1]) < 1.0e-10 --> true
+  @fact abs(momenta[2]) < 1.0e-10 --> true
+  @fact abs(momenta[3]) < 1.0e-10 --> true
+end
+
+
+
+
