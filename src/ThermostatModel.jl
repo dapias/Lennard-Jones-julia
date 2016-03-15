bd44b92e39fd73210ac9acf662cb76f058b3be89
dapias <dandroveros@gmail.com>
2016-03-14 22:16:05 -0600

    Add logistic distibution in ThermostatModel.jl

--- src/ThermostatModel.jl
+++ src/ThermostatModel.jl
@@ -16,13 +16,29 @@ Thermostat(Q, beta, p_eta) = Thermostat(Q, 0.0, p_eta, beta)
 ############Nosé-Hooover ###############33333
 @doc """Logarithm of the distibution in the extended space (f(w))"""->
 function logrhoextended(T::Thermostat)
-  -T.beta*T.p_eta^2/(2.*T.Q)   ##Q is a parameter that characterizes the distribution (standard deviation?)
+  -T.beta*T.p_eta^2/(2.*T.Q)
 end
 
 @doc """Friction coefficient f'(w)/f(w)"""->
 function friction(T::Thermostat)
   return -T.beta*T.p_eta/T.Q
 end
-####################################################3
+####################################################
+
+
+############Logistic distribution##############
+# function logrhoextended(T::Thermostat)
+#   z = T.p_eta-T.Q
+#   distribution = exp(z)/((1+exp(z))^2)
+#   log(distribution)
+# end
+
+# @doc """Friction coefficient f'(w)/f(w)"""->
+# function friction(T::Thermostat)
+#   z = T.p_eta-T.Q
+#   (1 - exp(z))/(1+exp(z))
+# end
+##############################################
+
 
 end
